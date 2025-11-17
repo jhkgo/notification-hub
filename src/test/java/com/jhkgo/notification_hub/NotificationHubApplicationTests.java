6052eaf1056c3c21784d0ff8da194bb3f3ca4a2e
jeongho <wjdgh9531@gmail.com>
2025-11-17 17:56:28 +0900

    init commit

---
+++ src/test/java/com/jhkgo/notification_hub/NotificationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhkgo.notification_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
